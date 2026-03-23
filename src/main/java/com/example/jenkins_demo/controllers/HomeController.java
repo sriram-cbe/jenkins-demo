c64c5d844c9364369dfe394ef1d36618c16305af
Sriram.B <sb2@wayfair.com>
2026-03-23 11:59:46 +0530

    First commit

---
+++ src/main/java/com/example/jenkins_demo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.jenkins_demo.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home/health")
+@AllArgsConstructor
+public class HomeController {
+
+    @GetMapping
+    public String health() {
+        return "The service is up and running..";
+    }
+}
